8847825687ff5454369c4ffaf81519bd20213f66
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-03-27 09:30:39 -0300

    FIX: testando git push

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,22 +1,22 @@
-import Fighter from '../Fighter';
-import Battle from './Battle';
+// import Fighter from '../Fighter';
+// import Battle from './Battle';
 
-export default class PVP extends Battle {
-  private _characterOne: Fighter;
-  private _characterTwo: Fighter;
-  constructor(characterOne: Fighter, characterTwo: Fighter) {
-    super(characterOne);
-    this._characterOne = characterOne;
-    this._characterTwo = characterTwo;
-  }
+// export default class PVP extends Battle {
+//   private _characterOne: Fighter;
+//   private _characterTwo: Fighter;
+//   constructor(characterOne: Fighter, characterTwo: Fighter) {
+//     super(characterOne);
+//     this._characterOne = characterOne;
+//     this._characterTwo = characterTwo;
+//   }
 
-  fight(): number {
-    while ((this._characterOne.lifePoints
-        && this._characterTwo.lifePoints) > 0) {    
-      this._characterTwo.attack(this._characterOne);
-      this._characterOne.attack(this._characterTwo);
-    }
+//   fight(): number {
+//     while ((this._characterOne.lifePoints
+//         && this._characterTwo.lifePoints) > 0) {    
+//       this._characterTwo.attack(this._characterOne);
+//       this._characterOne.attack(this._characterTwo);
+//     }
 
-    return this._characterOne.lifePoints === -1 ? -1 : 1;
-  }
-}
+//     return this._characterOne.lifePoints === -1 ? -1 : 1;
+//   }
+// }
